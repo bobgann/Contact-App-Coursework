784eda60a41307ebef0e6a3a97cc0f8b3e83575e
robertgann <bob.gann@state.co.us>
2022-10-14 17:56:15 -0600

    BLADE TEMPLATES

--- resources/views/contacts/index.blade.php
+++ resources/views/contacts/index.blade.php
@@ -12,39 +12,13 @@
                             <div class="d-flex align-items-center">
                                 <h2 class="mb-0">All Contacts</h2>
                                 <div class="ml-auto">
-                                    <a href="{{ route('contacts.create') }}" class="btn btn-success"><i class="fa fa-plus-circle"></i> Add New</a>
+                                    <a href="{{ route('contacts.create') }}" class="btn btn-success"><i
+                                            class="fa fa-plus-circle"></i> Add New</a>
                                 </div>
                             </div>
                         </div>
                         <div class="card-body">
-                            <div class="row">
-                                <div class="col-md-6"></div>
-                                <div class="col-md-6">
-                                    <div class="row">
-                                        <div class="col">
-                                            <select class="custom-select">
-                                                <option value="" selected>All Companies</option>
-                                                <option value="1">Company One</option>
-                                                <option value="2">Company Two</option>
-                                                <option value="3">Company Three</option>
-                                            </select>
-                                        </div>
-                                        <div class="col">
-                                            <div class="input-group mb-3">
-                                                <input type="text" class="form-control" placeholder="Search..." aria-label="Search..." aria-describedby="button-addon2">
-                                                <div class="input-group-append">
-                                                    <button class="btn btn-outline-secondary" type="button">
-                                                        <i class="fa fa-refresh"></i>
-                                                    </button>
-                                                    <button class="btn btn-outline-secondary" type="button" id="button-addon2">
-                                                        <i class="fa fa-search"></i>
-                                                    </button>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+                            @include('contacts._filter')
                             <table class="table table-striped table-hover">
                                 <thead>
                                 <tr>
@@ -57,20 +31,9 @@
                                 </tr>
                                 </thead>
                                 <tbody>
-                                <?php foreach ($contacts as $id => $contact):?>
-                                <tr>
-                                    <th scope="row">1</th>
-                                    <td>{{ $contact['name'] }}</td>
-                                    <td>{{ $contact['phone'] }}</td>
-                                    <td>alfred@test.com</td>
-                                    <td>Company one</td>
-                                    <td width="150">
-                                        <a href="{{ route('contacts.show' , $id) }}" class="btn btn-sm btn-circle btn-outline-info" title="Show"><i class="fa fa-eye"></i></a>
-                                        <a href="../../../../../Downloads/edomaru-contact_management_template-130cc506bd5c/form.html" class="btn btn-sm btn-circle btn-outline-secondary" title="Edit"><i class="fa fa-edit"></i></a>
-                                        <a href="#" class="btn btn-sm btn-circle btn-outline-danger" title="Delete" onclick="confirm('Are you sure?')"><i class="fa fa-times"></i></a>
-                                    </td>
-                                </tr>
-                                <?php endforeach ?>
+
+                                @each('contacts._contact',$contacts,'contact', 'contacts._empty')
+
                                 </tbody>
                             </table>
 
--- routes/web.php
+++ routes/web.php
@@ -16,20 +16,24 @@ use Illuminate\Support\Facades\Route;
 function getContacts()
 {
     return [
-        1 => ['name' => 'Name 1', 'phone' => '1234567890'],
-        2 => ['name' => 'Name 2', 'phone' => '2345678901'],
-        3 => ['name' => 'Name 3', 'phone' => '3456789012'],
+        1 => ['id'=> 1, 'name' => 'Name 1', 'phone' => '1234567890'],
+        2 => ['id'=> 2, 'name' => 'Name 2', 'phone' => '2345678901'],
+        3 => ['id'=> 3, 'name' => 'Name 3', 'phone' => '3456789012'],
     ];
 }
 
-Route::redirect('/','/contacts');
-/* This points home to 'contacts' instead of the index file
-*/
+Route::get('/', function () {
+    return view('welcome');
+})->name('public');
 
 Route::get('/contacts', function () {
+    $companies = [
+        1 => ['name' => 'Company One', 'contacts' => 3],
+        2 => ['name' => 'Company Two', 'contacts' => 5],
+    ];
     $contacts = getContacts();
 
-    return view('contacts.index', compact('contacts'));
+    return view('contacts.index', compact('contacts', 'companies'));
 })->name('contacts.index');
 
 Route::get('/contacts/create', function () {

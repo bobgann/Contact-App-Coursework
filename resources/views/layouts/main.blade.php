1febf07088f2338924eded73d2cbc801a44ab446
robertgann <bob.gann@state.co.us>
2022-10-14 14:15:20 -0600

    BLADE TEMPLATES

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <title>My Contact</title>
+    <title>@yield('title','Contact App')</title>
 
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Varela+Round">
     <!-- Bootstrap -->
@@ -17,7 +17,7 @@
 <!-- navbar -->
 <nav class="navbar navbar-expand-lg navbar-light">
     <div class="container">
-        <a class="navbar-brand text-uppercase" href="index.html">
+        <a class="navbar-brand text-uppercase" href="{{ route('contacts.index') }}">
             <strong>Contact</strong> App
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-toggler" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
@@ -28,7 +28,7 @@
         <div class="collapse navbar-collapse" id="navbar-toggler">
             <ul class="navbar-nav">
                 <li class="nav-item"><a href="#" class="nav-link">Companies</a></li>
-                <li class="nav-item active"><a href="#" class="nav-link">Contacts</a></li>
+                <li class="nav-item active"><a href="{{ route('contacts.index') }}" class="nav-link">Contacts</a></li>
             </ul>
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item mr-2"><a href="#" class="btn btn-outline-secondary">Login</a></li>
